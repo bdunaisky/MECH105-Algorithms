5548e91dbef4c03c142508266d12b86f8d5db185
bdunaisky <83365055+bdunaisky@users.noreply.github.com>
2021-04-28 17:49:47 -0600

    Updated derivative.approx
    Cleaned up file

--- derivativeApprox.m
+++ derivativeApprox.m
@@ -1,5 +1,6 @@
-function [output1] = derivativeApprox(x,x0,x1,x2,y0,y1,y2)
+function [estimate] = derivativeApprox(x,x0,x1,x2,y0,y1,y2)
 %derivativeApprox estimates the derivative of unequally spaced points.
-output1 = y0*(2*x-x1-x2)/((x0-x1)*(x0-x2))+y1*(2*x-x0-x2)/((x1-x0)*(x1-x2))+y2*(2*x-x0-x1)/((x2-x0)*(x2-x1));
+%You require at least three points. x is the point you are estimating the derivative around.
+estimate = y0*(2*x-x1-x2)/((x0-x1)*(x0-x2))+y1*(2*x-x0-x2)/((x1-x0)*(x1-x2))+y2*(2*x-x0-x1)/((x2-x0)*(x2-x1));
 end
 
